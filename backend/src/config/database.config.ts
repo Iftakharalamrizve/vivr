b83e0c1b2c16e223848cd1e1ee57e95e401d7399
iftakharalamrizve <iftakharalam1100@gmail.com>
2022-06-26 13:00:33 +0600

    udpate

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -1,9 +1,18 @@
 import { Sequelize } from 'sequelize';
+import {createClient} from 'redis';
 
+//sqlite db connection 
 const db = new Sequelize('app', '', '', {
 	storage: './database.sqlite',
 	dialect: 'sqlite',
 	logging: false,
 });
 
-export default db;
+//redis db connection 
+const client = createClient();
+client.on('error', (err) => console.log('Redis Client Error', err));
+client.connect();
+
+
+
+export default {db,client};
